97b1e512ed904d61df028bf7cd0e288165090cf3
yuta-machida-TEF <k024g0029@g.neec.ac.jp>
2025-04-15 11:29:51 +0900

    kamataEngineを入れた

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -2,8 +2,36 @@
 
 using namespace KamataEngine;
 
-void GameScene::Initialize() {}
+GameScene::~GameScene() {//~は、デストラクタのこといっている。デストラクタはプログラムが最後の時にプログラム名の削除を行う
+
+
+	delete sprite_;
+
+}
+
+void GameScene::Initialize() {
+
+	textureHandle_ = TextureManager::Load("banana.png"); 
+
+	//スプライトインスタンスの生成
+	sprite_ = Sprite::Create(textureHandle_, {100,50});
+
+	//delete sprite_;
+}
 
 void GameScene::Update() {}
 
-void GameScene::Draw() {}
+void GameScene::Draw() {
+
+	//DirecitXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	//スプライト描画前処理
+	Sprite::PreDraw(dxCommon->GetCommandList());
+
+	sprite_->Draw();
+
+	//スプライト描画後処理
+	Sprite::PostDraw();
+
+}
--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -5,7 +5,12 @@ class GameScene
 {
 public:
 	//初期化
+	~GameScene();//~はdelete
 	void Initialize();
+	uint32_t textureHandle_ = 0;
+
+	//スプライト
+	KamataEngine::Sprite* sprite_ = nullptr;
 
 	//更新
 	void Update();
